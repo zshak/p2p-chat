a44940e67c6dbc6c2820ff4e6c930a4d4e4401b9
zuka <zukashakulashvili2002@gmail.com>
2025-04-06 18:32:21 +0400

    change dht mode to auto

--- daemon/cmd/p2p-chat-daemon/discovery/dht.go
+++ daemon/cmd/p2p-chat-daemon/discovery/dht.go
@@ -13,7 +13,6 @@ import (
 	"github.com/libp2p/go-libp2p/core/host"
 	p2pPeer "github.com/libp2p/go-libp2p/core/peer"
 	"github.com/libp2p/go-libp2p/p2p/discovery/routing"
-	relayv2 "github.com/libp2p/go-libp2p/p2p/protocol/circuitv2/relay"
 )
 
 var defaultBootstrapPeers = addrInfosFromStrings([]string{
@@ -49,16 +48,16 @@ func SetupGlobalDiscovery(ctx context.Context, node host.Host, shouldUsePublicBt
 		log.Println("Setting up global DHT discovery with private bootstrap peers...")
 	}
 
-	resources := relayv2.DefaultResources()
-	resources.MaxReservations = 256
-	_, err := relayv2.New(node, relayv2.WithResources(resources))
-	if err != nil {
-		panic(err)
-	}
+	//resources := relayv2.DefaultResources()
+	//resources.MaxReservations = 256
+	//_, err := relayv2.New(node, relayv2.WithResources(resources))
+	//if err != nil {
+	//	panic(err)
+	//}
 
 	// Create a DHT client mode or server mode based on need
 	kadDHT, err := dht.New(ctx, node,
-		dht.Mode(dht.ModeServer),
+		dht.Mode(dht.ModeAuto),
 		dht.ProtocolPrefix(dhtProtocol),
 		dht.BootstrapPeers(defaultBootstrapPeers...),
 	)
